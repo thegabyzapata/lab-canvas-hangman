e35140504152d9912507cc8c1bb22a06182727de
Gabriela Zapata <gabriela.zapata@unir.net>
2019-12-10 17:25:26 +0100

    start coding canvas.js

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,15 +2,16 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.secretWord = secretWord;
   }
 
   createBoard() {
-
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    
   }
 
   drawLines() {
-
+    
   }
 
   writeCorrectLetter(index) {
