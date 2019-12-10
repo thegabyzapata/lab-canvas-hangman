968f084e1f5e546a9626b17754f642fbc25333e8
Gabriela Zapata <gabriela.zapata@unir.net>
2019-12-10 16:04:40 +0100

    Complete methods

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -10,33 +10,42 @@ class Hangman {
   }
 
   getWord() {
-    
+    return this.words[Math.floor(Math.random() * this.words.length)];
   }
 
   checkIfLetter(keyCode) {
-
+    return keyCode >= 65 && keyCode <= 90;
   }
 
   checkClickedLetters(key) {
-
+    return !this.letters.includes(key);
   }
 
   addCorrectLetter(i) {
-
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    this.letters.push(this.secretWord[i]);
+    return this.checkWinner();
   }
 
   addWrongLetter(letter) {
-
+    this.letters.push(letter);
+    this.errorsLeft --;
+    return this.checkGameOver();
   }
 
   checkGameOver() {
-
+    if (this.errorsLeft === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkWinner() {
-
+    if (this.guessedLetter.length === this.secretWord.length){
+      return true;
+    }
   }
-
 }
 
 document.getElementById('start-game-button').onclick = () => {
